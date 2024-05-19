e10f6f77504475592b61fb3740c7c9ef252a2a2d
Sujit Sundar Amin <143650989+sujitaminusc@users.noreply.github.com>
2024-05-18 20:26:52 -0700

    SpringBoot Web Services

--- Spring Boot/restful-web-services/src/main/java/com/example/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ Spring Boot/restful-web-services/src/main/java/com/example/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.restfulwebservices.exception;
 
 import com.example.restfulwebservices.user.UserNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -19,14 +21,19 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomizedResponseEntityExceptionHandler.class);
+
+
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        logger.error("Exception: ", ex);
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+        logger.warn("UserNotFoundException: ", ex);
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
--- Spring Boot/restful-web-services/src/main/java/com/example/restfulwebservices/user/UserJPAResource.java
+++ Spring Boot/restful-web-services/src/main/java/com/example/restfulwebservices/user/UserJPAResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+// this is controller.
 @RestController
 public class UserJPAResource {
 
