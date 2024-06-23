ac730a7edb0193c6f99601037b413ec7b2ec687a
Sujit Sundar Amin <143650989+sujitaminusc@users.noreply.github.com>
2024-06-22 18:21:50 -0700

    commit

--- src/ElevatorSystem/src/ElevatorCar.java
+++ src/ElevatorSystem/src/ElevatorCar.java
@@ -2,6 +2,14 @@ import button.Direction;
 import panel.Display;
 import panel.ElevatorPanel;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+enum Algorithm {
+    SSTF,
+    LOOK
+}
 public class ElevatorCar {
     private int id;
     private Door door;
@@ -11,6 +19,7 @@ public class ElevatorCar {
     private int currentFloor;
     private int targetFloor;
     private int capacity;
+    private List<Integer> floorRequests;
 
     public ElevatorCar(int id, int numberOfFloors, Display display, ElevatorPanel panel) {
         this.id = id;
@@ -21,9 +30,11 @@ public class ElevatorCar {
         currentFloor = 1; // Assume ground or first floor as initial floor
         targetFloor = 1;
         capacity = numberOfFloors;
+        floorRequests = new ArrayList<>();
     }
-
+    // FCFS (First-Come, First-Served)
     public void move() {
+
         if (targetFloor > currentFloor) {
             state = ElevatorState.UP;
             while (currentFloor < targetFloor) {
@@ -39,6 +50,105 @@ public class ElevatorCar {
         }
         stop();
     }
+
+
+    public void requestFloor(int floor) {
+        floorRequests.add(floor);
+        Collections.sort(floorRequests); // Keep requests sorted
+    }
+
+    public void moveSSTF(Algorithm algorithm) {
+        while (!floorRequests.isEmpty()) {
+            int closestFloor = findClosestFloor();
+            if (closestFloor > currentFloor) {
+                state = ElevatorState.UP;
+                display.direction = Direction.UP;
+                while (currentFloor < closestFloor) {
+                    currentFloor++;
+                    display.updateElevatorDisplay(currentFloor, capacity, Direction.UP);
+                }
+            } else if (closestFloor < currentFloor) {
+                state = ElevatorState.DOWN;
+                display.direction = Direction.DOWN;
+                while (currentFloor > closestFloor) {
+                    currentFloor--;
+                    display.updateElevatorDisplay(currentFloor, capacity, Direction.DOWN);
+                }
+            }
+            stop();
+            floorRequests.remove(Integer.valueOf(closestFloor));
+        }
+    }
+
+    // Elevator Algorithm (SCAN):  This strategy moves the elevator in one direction, serving all requests in that direction,
+    // then reverses direction and serves requests in the opposite direction, similar to the movement of an elevator.
+    // The provided code does not account for continuous movement in one direction
+    // before reversing; it directly moves to the specified targetFloor.
+    // LOOK Similar to the Elevator Algorithm but instead of moving to the end of the direction,
+    // it only goes as far as the last request in that direction.
+    // Again, the provided code does not check for other requests but directly moves to the specified targetFloor.
+    private void moveLOOK() {
+        while (!floorRequests.isEmpty()) {
+            if (display.direction == Direction.UP) {
+                Iterator<Integer> iterator = floorRequests.iterator();
+                while (iterator.hasNext()) {
+                    int floor = iterator.next();
+                    if (floor > currentFloor) {
+                        state = ElevatorState.UP;
+                        while (currentFloor < floor) {
+                            currentFloor++;
+                            display.updateElevatorDisplay(currentFloor, capacity, Direction.UP);
+                        }
+                        stop();
+                        iterator.remove();
+                    }
+                }
+                display.direction = Direction.DOWN;
+            } else {
+                Iterator<Integer> iterator = floorRequests.iterator();
+                while (iterator.hasNext()) {
+                    int floor = iterator.next();
+                    if (floor < currentFloor) {
+                        state = ElevatorState.DOWN;
+                        while (currentFloor > floor) {
+                            currentFloor--;
+                            display.updateElevatorDisplay(currentFloor, capacity, Direction.DOWN);
+                        }
+                        stop();
+                        iterator.remove();
+                    }
+                }
+                display.direction = Direction.UP;
+            }
+        }
+    }
+
+    private int findClosestFloor() {
+        int closestFloor = floorRequests.get(0);
+        int minDistance = Math.abs(currentFloor - closestFloor);
+        for (int floor : floorRequests) {
+            int distance = Math.abs(currentFloor - floor);
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestFloor = floor;
+            }
+        }
+        return closestFloor;
+    }
+
+    // Shortest Seek Time First (SSTF) This algorithm serves
+    // the request closest to the current position of the elevator, minimizing movement.
+    // The provided code does not check for the closest target but instead moves directly to the specified targetFloor.
+    public void moveSSTF() {
+
+    }
+
+//    public void moveSCAN() {
+//
+//    }
+
+
+
 
     public void stop() {
         state = ElevatorState.IDLE;
@@ -69,7 +179,10 @@ public class ElevatorCar {
         return id;
     }
 
-    public void addPassenger(int capacity) {
+    public void addPassenger(int capacity) throws Exception {
+        if (capacity + this.capacity > 8) {
+            throw new Exception("Full capacity");
+        }
         this.capacity += capacity;
         display.updateElevatorDisplay(currentFloor, this.capacity, Direction.UP);
     }
--- src/ElevatorSystem/src/Main.java
+++ src/ElevatorSystem/src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         // Create a building with 10 floors and 2 elevator cars
-        Building building = Building.getInstance();
-        building.addFloors(8);
-        building.addElevatorCars(3);
+        try {
+            Building building = Building.getInstance();
+            building.addFloors(8);
+            building.addElevatorCars(3);
 
-        // Retrieve the elevator system instance
-        ElevatorSystem elevatorSystem = ElevatorSystem.getInstance();
+            // Retrieve the elevator system instance
+            ElevatorSystem elevatorSystem = ElevatorSystem.getInstance();
 
-        // Get the first elevator car and set a target floor
+            // Get the first elevator car and set a target floor
 //        ElevatorCar elevator1 = building.getElevatorCar(1);
 //        elevator1.setTargetFloor(5);
 //        elevator1.move();
@@ -16,19 +17,23 @@ public class Main {
 //        // Add passengers to the elevator
 //        elevator1.addPassenger(3);
 
-        // Get the second elevator car and set a target floor
-        ElevatorCar elevator2 = building.getElevatorCar(2);
-        elevator2.setTargetFloor(8);
-        elevator2.move();
+            // Get the second elevator car and set a target floor
+            ElevatorCar elevator2 = building.getElevatorCar(2);
+            elevator2.addPassenger(3);
+            elevator2.setTargetFloor(8);
+            elevator2.move();
+            elevator2.addPassenger(1);
+            elevator2.removePassenger(1);
+            elevator2.setTargetFloor(2);
+            elevator2.move();
 
-        elevator2.removePassenger(1);
-        elevator2.setTargetFloor(2);
-        elevator2.move();
+            // Remove passengers from the elevator
+            elevator2.removePassenger(1);
 
-        // Remove passengers from the elevator
-        elevator2.removePassenger(1);
-
-        // Monitor the elevators
-        elevatorSystem.monitoring();
+            // Monitor the elevators
+            elevatorSystem.monitoring();
+        } catch (Error | Exception e) {
+            e.printStackTrace();
+        }
     }
 }
